0abad5f24f5f3a7531e7b0c42d26d7ee047e4133
Monika <raciam@oregonstate.edu>
2023-09-11 22:54:17 -0500

    Add GET and POST routes for /api/users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,51 @@
 const { User } = require('../models');
 
 const userController = {
-   
+    // Get all users
+    async getUsers(req, res) {
+        try {
+            const userData = await User.find({})
+              .select('-__v');  // exclude version key
+            res.json(userData);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async getSingleUser(req, res) {
+        try {
+            const userData = await User.findOne({ _id: req.params.userId });
+
+            if (!userData) {
+                return res.status(404).json({ message: 'No user with that ID' });
+            }
+
+            res.json(userData);
+        } catch (err) {
+            res.status(500).json(err);
+        } 
+    },
+
+    // Create a user
+    async createUser(req, res) {
+        try {
+            const userData = await User.create(req.body);
+            res.json(userData);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async updateUser(req, res) {
+        try {
+            const userData = await User.findOneAndUpdate(
+                { _id: req.params.id },
+            )
+
+        } catch (err) {
+
+        }
+    }
 }
 
 module.exports = userController;
